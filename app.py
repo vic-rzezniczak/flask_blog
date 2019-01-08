1f6d5199c99707da8239f9e11cc3c67bfaa398b5
unknown <vic.rzezniczak@gmail.com>
2019-01-08 20:49:44 +0100

    first template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 import re
 
@@ -12,15 +12,9 @@ def home():
 
 @app.route("/hello/<name>")
 def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    #formatted_now = now.strftime("%A, %d %B, %Y at %X")
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match('[a-zA-Z]+', name)
+    #content = "<strong>Hello there, " + name + "!</strong> It's " + formatted_now
+    #content = f'Hello there, {name}! It\'s {formatted_now}'
 
-    if match_object: clean_name = match_object.group(0)
-    else: clean_name = 'Friend'
-
-    content = f'Hello there, {clean_name}! It\'s {formatted_now}'
-    return content
+    return render_template('index.html', title='Hello, Flask', name=name, date=datetime.now())
